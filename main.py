e3054ba74321b4344b14210d217866c21e8f3509
Md Abul Hasnat <opqclick@gmail.com>
2023-10-17 14:34:16 +0600

    updates

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Form
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 import face_recognition
 import requests
-from PIL import Image
-from io import BytesIO
 
 app = FastAPI()
 
@@ -13,40 +11,37 @@ def find_face_encodings(image_url):
     response = requests.get(image_url)
     response.raise_for_status()  # Check for any request errors
 
-    # Open the downloaded image with Pillow (PIL)
-    image = Image.open(BytesIO(response.content))
+    # Convert the image content to bytes
+    image_bytes = response.content
 
-    # Convert the Pillow image to a numpy array for face_recognition
-    image_array = face_recognition.load_image_file(BytesIO(response.content))
+    # Process the image bytes
+    image_array = face_recognition.face_encodings(image_bytes)
 
-    # Get face encodings from the image
-    face_enc = face_recognition.face_encodings(image_array)
-    
-    if not face_enc:
+    if not image_array:
         raise ValueError("No faces found in the provided image")
 
-    return face_enc[0]
+    return image_array[0]
 
-@app.get("/")
-async def read_root(
-    image_url_1: str = Query(..., description="URL of the first image"),
-    image_url_2: str = Query(..., description="URL of the second image"),
+@app.post("/")
+async def compare_images(
+    id_card_photo_url: str = Form(..., description="ID Card Photo URL"),
+    recent_camera_photo_url: str = Form(..., description="Recent Camera Photo URL"),
 ):
     try:
-        # Download and process the images from the provided URLs
-        image_1 = find_face_encodings(image_url_1)
-        image_2 = find_face_encodings(image_url_2)
+        # Process the images from the provided URLs
+        id_card_face_encoding = find_face_encodings(id_card_photo_url)
+        recent_camera_face_encoding = find_face_encodings(recent_camera_photo_url)
 
-        is_same = face_recognition.compare_faces([image_1], image_2)[0]
+        is_same = face_recognition.compare_faces([id_card_face_encoding], recent_camera_face_encoding)[0]
 
         if is_same:
-            distance = face_recognition.face_distance([image_1], image_2)
+            distance = face_recognition.face_distance([id_card_face_encoding], recent_camera_face_encoding)
             distance = round(distance[0] * 100)
             accuracy = 100 - round(distance)
 
-            response = {"result": "The images are same", "accuracy": accuracy}
+            response = {"result": "The images are the same", "accuracy": accuracy}
         else:
-            response = {"result": "The images are not same"}
+            response = {"result": "The images are not the same"}
 
         return JSONResponse(content=jsonable_encoder(response))
     except Exception as e:
